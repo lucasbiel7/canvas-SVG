27d48e0d5c59bbd1390690e241bc3620793e4869
lucasbiel7 <lucasbiel7@icloud.com>
2018-09-16 20:47:10 -0300

    Ajuste no trabalho adicionando evento de drag and drop audio video e iframe

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
 
-function getContext(){
+if (localStorage.getItem('nome') == null) {
+    localStorage.setItem("nome", window.prompt("Digite seu nome", ""));
+}
+
+function atualizarNome() {
+    console.log("tentando atualizar nome");
+    var spanNome = document.getElementById("nome");
+    spanNome.innerHTML = localStorage.getItem("nome");
+}
+
+
+function getContext() {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
     return context;
 }
 
-function clearContext(){
+function clearContext() {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function desenharRetangulos(){
+function desenharRetangulos() {
     clearContext();
     getContext().beginPath();
-    getContext().rect(0,0,200,200);
-    for(let i = 0 ; i <10 ; i++){
-        getContext().fillStyle = "red";
-        getContext().fillRect(10*i,10*i,50,50);
-        getContext().rect(10*i,10*i,50,50);
-        getContext().fillStyle = "yellow";
-        getContext().fillRect(10*i+60,10*i+60,50,50);
-        getContext().rect(10*i+60,10*i+60,50,50);
+    getContext().rect(0, 0, 200, 200);
+    for (let i = 0; i < 16; i++) {
+        getContext().rect(15 + parseInt(i / 4) * 50, 15 + (i % 4) * 50, 25, 25);
     }
     getContext().stroke();
     getContext().closePath();
 }
 
-function desenharCirculos(){
+function desenharCirculos() {
+    clearContext();
+    for (let i = 0; i < 10; i++) {
+        getContext().beginPath();
+        getContext().arc(150 + i, 150 + i, 100 - i * 10, 0, 2 * Math.PI);
+        getContext().stroke();
+        getContext().closePath();
+    }
+}
+
+function desenharCirculoGradient() {
     clearContext();
     getContext().beginPath();
-    for(let i = 0; i <10 ; i++){
-        getContext().arc(150+i, 150+i ,100-i*10 , 0 ,  2*Math.PI);
-    }
+    var lg = getContext().createLinearGradient(150, 40, 100, 200);
+    lg.addColorStop(0, "green");
+    lg.addColorStop(1, "white");
+    getContext().fillStyle = lg;
+    getContext().arc(100, 100, 50, 0, 2 * Math.PI);
+    getContext().fill();
     getContext().stroke();
     getContext().closePath();
+}
+
+function permitirDropar(ev) {
+    ev.preventDefault();
+}
+
+function dropQualidades(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.innerHTML += data + "<br/>";
+}
+
+function moverQualidade(ev) {
+    ev.dataTransfer.setData("text", ev.target.innerHTML);
 }
